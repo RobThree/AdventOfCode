6b681b6aa558491f7ef500120c8210747d4815c0
RobThree <rob@devcorner.nl>
2015-12-07 02:33:20 +0100

    Day 1

---
+++ Day01.01/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"Floor: {File.ReadAllText("input.txt").Sum(c => c == '(' ? 1 : -1)}");
+    }
+}
---
+++ Day01.02/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var f = 0;
+        Console.WriteLine($@"Floor: {File.ReadAllText("input.txt")
+            .Select((c, i) => { f += c == '(' ? 1 : -1; return new { f, i }; })
+            .First(o => o.f == -1).i + 1}");
+    }
+}
+
