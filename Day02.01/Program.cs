bad25c7e460e9d689eb7f6a563705675931cee9d
RobThree <rob@devcorner.nl>
2015-12-07 17:31:52 +0100

    Day 2

---
+++ Day02.01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine($@"Result: {File.ReadAllLines("input.txt")
+            .Select(l => l.Split('x')
+                .Select(i => int.Parse(i)).ToArray()
+            )
+            .Select(d => new { l = d[0], w = d[1], h = d[2] })
+            .Select(d => new { p1 = d.l * d.w, p2 = d.w * d.h, p3 = d.h * d.l })
+            .Select(d => d.p1 * 2 + d.p2 * 2 + d.p3 * 2 + Math.Min(Math.Min(d.p1, d.p2), d.p3))
+            .Sum()} square feet");
+    }
+}
---
+++ Day02.02/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine($@"Result: {File.ReadAllLines("input.txt")
+            .Select(l => l.Split('x')
+                .Select(i => int.Parse(i)).ToArray()
+            )
+            .Select(d => d.OrderBy(i => i).Take(2).Sum() * 2 + d[0] * d[1] * d[2])
+            .Sum()} feet");
+    }
+}
