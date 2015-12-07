ff14ae437289eeb48fcdc2e17df7ed2261299696
RobThree <rob@devcorner.nl>
2015-12-07 21:12:21 +0100

    Day 5

---
+++ Day05.01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine($@"{File.ReadAllLines("input.txt").Count(l =>
+            l.Count(c => "aeiou".IndexOf(c) >= 0) > 2
+                && l.Skip(1).Where((c, i) => c == l[i]).Any()
+                && !(new string[] { "ab", "cd", "pq", "xy" }).Any(n => l.IndexOf(n) >= 0)
+        )} strings");
+    }
+}
---
+++ Day05.02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine($@"{File.ReadAllLines("input.txt").Where(
+            l => l.ToPairs()
+                .Select((p, i) => new { p, i })
+                .GroupBy(p => p.p)
+                .Where(g => g.OrderBy(p => p.i).Last().i - g.OrderBy(p => p.i).First().i > 1)
+                .Any()
+        ).Where(l=> l.Skip(2).Select((c, i) => new { c, i }).Any(v => v.c == l[v.i])).Count()} strings");
+    }
+}
+
+static class Helpers
+{
+    public static IEnumerable<string> ToPairs(this string c)
+    {
+        var p = c[0];
+        foreach (var i in c.Skip(1))
+        {
+            yield return p.ToString() + i.ToString();
+            p = i;
+        }
+    }
+}
