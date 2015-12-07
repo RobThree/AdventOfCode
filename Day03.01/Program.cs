63332db843221cde11b636eefe33a57dea4043ba
RobThree <rob@devcorner.nl>
2015-12-07 18:41:48 +0100

    Day 3

---
+++ Day03.01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Drawing;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var positions = new ConcurrentDictionary<Point, int>();
+        var current = new Point(0, 0);
+        positions.TryAdd(current, 1);
+        foreach (var c in File.ReadAllText("input.txt"))
+        {
+            switch (c)
+            {
+                case '>': current.Offset(1, 0); break;
+                case '<': current.Offset(-1, 0); break;
+                case '^': current.Offset(0, 1); break;
+                case 'v': current.Offset(0, -1); break;
+            }
+            positions.AddOrUpdate(current, 1, (p, v) => ++v);
+        }
+        Console.WriteLine($"{positions.Count} houses");
+    }
+}
---
+++ Day03.02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Drawing;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var positions = new ConcurrentDictionary<Point, int>();
+        var current = new[] { new Point(0, 0), new Point(0, 0) };
+        var offset = new Point(0, 0);
+        var i = 0;
+        positions.TryAdd(offset, 1);
+        foreach (var c in File.ReadAllText("input.txt"))
+        {
+            switch (c)
+            {
+                case '>': current[i % 2].Offset(1, 0); break;
+                case '<': current[i % 2].Offset(-1, 0); break;
+                case '^': current[i % 2].Offset(0, 1); break;
+                case 'v': current[i % 2].Offset(0, -1); break;
+            }
+            positions.AddOrUpdate(current[i++ % 2], 1, (p, v) => ++v);
+        }
+        Console.WriteLine($"{positions.Count} houses");
+    }
+}
