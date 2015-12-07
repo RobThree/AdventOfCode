cdfe8de7e1adf387f9c845663ae71c5b936bf2b8
RobThree <rob@devcorner.nl>
2015-12-07 19:01:53 +0100

    Day 4

---
+++ Day04.01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //TODO: Use all cores for speedup...
+        var h = MD5.Create();
+        var input = Encoding.ASCII.GetBytes("yzbqklnj");
+        var i = 0;
+        while (true)
+        {
+            var r = h.ComputeHash(input.Concat(Encoding.ASCII.GetBytes(i++.ToString())).ToArray());
+            if (r[0] == 0 && r[1] == 0 && (r[2] & 0xF0) == 0)
+                break;
+        }
+        Console.WriteLine(--i);
+    }
+}
