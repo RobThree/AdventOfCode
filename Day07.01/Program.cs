bdf49ac042e72c0a9eaa22fb2a8c81a0095a9abc
RobThree <rob@devcorner.nl>
2015-12-08 23:46:33 +0100

    Day 7

---
+++ Day07.01/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    private static Dictionary<string, ushort> values = new Dictionary<string, ushort>();
+    private static Dictionary<string, Func<ushort>> expr = new Dictionary<string, Func<ushort>>();
+    // Little more readable decoder than in part 2
+    private static Dictionary<Regex, Action<Match>> decoder = new Dictionary<Regex, Action<Match>>
+    {
+
+        [new Regex(@"^(\w+) -> (\w+)$", RegexOptions.Compiled)] = (m) => { expr[m.Groups[2].Value] = () => EvalValue(m.Groups[1].Value); },                 // Assignment x -> y
+        [new Regex(@"^NOT (\w+) -> (\w+)$", RegexOptions.Compiled)] = (m) => { expr[m.Groups[2].Value] = () => (ushort)~EvalValue(m.Groups[1].Value); },    // NOT x -> y
+        [new Regex(@"^(\w+) (AND|OR|LSHIFT|RSHIFT) (\w+) -> (\w+)$", RegexOptions.Compiled)] = (m) =>                                                       // Expression x OPERATOR y -> z
+        {
+            switch (m.Groups[2].Value)
+            {
+                case "AND": expr[m.Groups[4].Value] = () => (ushort)(EvalValue(m.Groups[1].Value) & EvalValue(m.Groups[3].Value)); break;
+                case "OR": expr[m.Groups[4].Value] = () => (ushort)(EvalValue(m.Groups[1].Value) | EvalValue(m.Groups[3].Value)); break;
+                case "LSHIFT": expr[m.Groups[4].Value] = () => (ushort)(EvalValue(m.Groups[1].Value) << EvalValue(m.Groups[3].Value)); break;
+                case "RSHIFT": expr[m.Groups[4].Value] = () => (ushort)(EvalValue(m.Groups[1].Value) >> EvalValue(m.Groups[3].Value)); break;
+            }
+        }
+    };
+
+    static void Main(string[] args)
+    {
+        Match m = null;
+        foreach (var l in File.ReadAllLines("input.txt"))
+            decoder.First(kv => { m = kv.Key.Match(l); return m.Success; }).Value(m);   // Find first function that matches syntax and execute it
+        Console.WriteLine($"Value: {expr["a"]()}");
+    }
+
+    public static ushort EvalValue(string value)
+    {
+        ushort tmp;
+        if (ushort.TryParse(value, out tmp))
+            return tmp;
+
+        if (!values.ContainsKey(value))
+            values[value] = expr[value]();
+        return values[value];
+    }
+}
---
+++ Day07.02/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    private static Dictionary<string, ushort> values = new Dictionary<string, ushort>();
+    private static Dictionary<string, Func<ushort>> expr = new Dictionary<string, Func<ushort>>();
+    // Functionally same as in part 1 decoder; however this time we don't use a switch case but have more regexes
+    private static Dictionary<Regex, Action<Match>> decoder = new Dictionary<Regex, Action<Match>>
+    {
+        [new Regex(@"^(\w+) -> (\w+)$", RegexOptions.Compiled)] = (m) => { expr[m.Groups[2].Value] = () => EvalValue(m.Groups[1].Value); },                                                         // Assignment x -> y
+        [new Regex(@"^NOT (\w+) -> (\w+)$", RegexOptions.Compiled)] = (m) => { expr[m.Groups[2].Value] = () => (ushort)~EvalValue(m.Groups[1].Value); },                                            // NOT x -> y
+        [new Regex(@"^(\w+) OR (\w+) -> (\w+)$", RegexOptions.Compiled)] = (m) => { expr[m.Groups[3].Value] = () => (ushort)(EvalValue(m.Groups[1].Value) | EvalValue(m.Groups[2].Value)); },       // x OR y -> z
+        [new Regex(@"^(\w+) AND (\w+) -> (\w+)$", RegexOptions.Compiled)] = (m) => { expr[m.Groups[3].Value] = () => (ushort)(EvalValue(m.Groups[1].Value) & EvalValue(m.Groups[2].Value)); },      // x AND y -> z
+        [new Regex(@"^(\w+) LSHIFT (\w+) -> (\w+)$", RegexOptions.Compiled)] = (m) => { expr[m.Groups[3].Value] = () => (ushort)(EvalValue(m.Groups[1].Value) << EvalValue(m.Groups[2].Value)); },  // x LSHIFT y -> z
+        [new Regex(@"^(\w+) RSHIFT (\w+) -> (\w+)$", RegexOptions.Compiled)] = (m) => { expr[m.Groups[3].Value] = () => (ushort)(EvalValue(m.Groups[1].Value) >> EvalValue(m.Groups[2].Value)); },  // x RSHIFT y -> z
+    };
+
+    static void Main(string[] args)
+    {
+        Match m = null;
+        foreach (var l in File.ReadAllLines("input.txt"))
+            decoder.First(kv => { m = kv.Key.Match(l); return m.Success; }).Value(m);   // Find first function that matches syntax and execute it
+
+        var result = expr["a"]();
+        values.Clear();
+        values["b"] = result;
+        Console.WriteLine($"Value: {expr["a"]()}");
+    }
+
+    public static ushort EvalValue(string value)
+    {
+        ushort tmp;
+        if (ushort.TryParse(value, out tmp))
+            return tmp;
+
+        if (!values.ContainsKey(value))
+            values[value] = expr[value]();
+        return values[value];
+    }
+}
