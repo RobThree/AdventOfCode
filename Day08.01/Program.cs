6c502eceb5b3277070b03e848895d000c8fb5a33
RobThree <rob@devcorner.nl>
2015-12-09 00:27:14 +0100

    Day 8

---
+++ Day08.01/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var r = new Regex(@"\\x[0-9a-f]{2}|\\""|\\\\");
+        Console.WriteLine($"Length: {File.ReadAllLines("input.txt").Sum(l => l.Length - (r.Replace(l, "☃").Length - 2))}");
+    }
+}
---
+++ Day08.02/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var r = new Regex(@"(\\|"")");
+        Console.WriteLine($"Length: {File.ReadAllLines("input.txt").Sum(l => 2 + (r.Replace(l, "\\$1").Length - l.Length))}");
+    }
+}
